59409ef77b14bbca9683d90048ff1f7cd1bea9de
ahmza48 <ahmza48.dev@gmail.com>
2024-01-25 20:52:34 +0500

    created a  custom url to get all the employees of a specific company

--- companyapi/api/models.py
+++ companyapi/api/models.py
@@ -12,6 +12,9 @@ class Company(models.Model):
     added=models.DateTimeField(auto_now_add=True)
     active=models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name+'-'+self.location
+
 # Creating Employee Model
 class Employee(models.Model):
     name=models.CharField(max_length=100)
